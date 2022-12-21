b9178acacba8bd776afbdcbfc8465076aba757c3
Rakshitnajbile <42490094+Rakshitnajbile@users.noreply.github.com>
2022-12-22 00:07:38 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,31 @@
+<?php
+        include 'login.php';
+        ?>
+ 
+<footer>
+    <div class="mt-5 pt-5 pb-5 footer">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-4 col-xs-12 ">
+      <h3 style="color:white">Information</h3>
+      <p class="pr-5 text-white-50"><a href="about.php"><span style="color:white">About Us</span></a> </p>
+      <p><a href="contact.php"><span style="color:white">Contact Us</span></a></p>
+    </div>
+    <div class="col-lg-4 col-xs-12 " style="color:white">
+      <h3 class="mt-lg-0 mt-sm-3">My accounts</h3>
+        
+         <p><a href="#" data-toggle="modal" data-target="#loginmodal" ><span style="color:white">Log In</span></a></p>
+         <p><a href="signup.php"><span style="color:white">Sign Up</span></a></p> 
+        
+    </div>
+    <div class="col-lg-4 col-xs-12 " style="color:white">
+      <h3 class="mt-lg-0 mt-sm-4">Contact us</h3>
+      <p>CONTACT: +91-123-000000</p>
+      
+    </div>
+  </div>
+  
+</div>
+</div>
+</footer>
+  
